ccbe348f2179634ff6f6c7bf8b0d95d3245c9034
Ilia Kortchinski <kortchinskii@gmail.com>
2025-07-07 20:51:25 +0300

    refactor: add arrays of objects for rendering items and menu

---
+++ scripts/items.js
@@ -0,0 +1,29 @@
+const listContainer = document.querySelector('.list-container');
+
+export const renderItems = (items) => {
+  items.forEach((item) => {
+    const itemArtickle = document.createElement('article');
+    itemArtickle.classList.add('list-item');
+
+    itemArtickle.innerHTML = `
+            <a href="#">
+                <div class="list-item-header">
+                  <div class="image">
+                    <img src=${item.image} alt=${item.title} />
+                  </div>
+                  <div><h1 class="list-item-title">${item.title}</h1></div>
+                </div>
+                <div>
+                  <p class="list-item-description">
+                    ${item.description}
+                  </p>
+                  <div>
+                    <span class="list-item-version">${item.version}</span>
+                    <span class="additional-versions">${item.otherVersions}</span>
+                  </div>
+                </div>
+            </a>`;
+
+    listContainer.appendChild(itemArtickle);
+  });
+};
---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+import { links, projects } from './data.js';
+import { renderMenu } from './menu.js';
+import { renderItems } from './items.js';
+
+renderMenu(links);
+renderItems(projects);
