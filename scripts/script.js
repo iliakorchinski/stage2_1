639b138823070af7e5461f2a5098681008c901ed
Ilia Kortchinski <kortchinskii@gmail.com>
2025-07-08 14:28:31 +0300

    feat: implement open/hide functionality for side panel and configured eslint

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,19 @@
 import { links, projects } from './data.js';
-import { renderMenu } from './menu.js';
 import { renderItems } from './items.js';
+import { renderMenu } from './menu.js';
+import { toggleBurgerMenu } from './mobile-menu.js';
 
 const listContainer = document.querySelector('.list-container');
 
+window.addEventListener('resize', () => {
+  const element = document.querySelector('.side-menu-navigation');
+  if (window.innerWidth > 900) {
+    element.style.display = 'none';
+  } else {
+    element.style.display = 'block';
+  }
+});
+
 renderMenu(links);
 renderItems(projects, listContainer);
+toggleBurgerMenu();
