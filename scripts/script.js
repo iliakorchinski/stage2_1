039dbe3e845d8bbdc1416b8e1b5513ae3b116c7d
Ilia Kortchinski <kortchinskii@gmail.com>
2025-07-09 16:02:57 +0300

    refactor: split renderItems function into smaller reusable functions, brought the functions to arrow type

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,7 @@ import { links, projects } from './data.js';
 import { renderMenu } from './menu.js';
 import { renderItems } from './items.js';
 
+const listContainer = document.querySelector('.list-container');
+
 renderMenu(links);
-renderItems(projects);
+renderItems(projects, listContainer);
